32cd8386dcee8ec010f1ff09749aad543ecffc58
Manhad Khan <manhad.majin@outlook.com>
2023-05-27 19:22:37 +0530

    added a playRound function which plays a single round.

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
   }
-  
+
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return "You Win! Rock beats Scissors";
+        } else {
+            return "You Lose! Paper beats Rock";
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return "You Win! Paper beats Rock";
+        } else {
+            return "You Lose! Scissors beats Paper";
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return "You Win! Scissors beats Paper";
+        } else {
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+}
